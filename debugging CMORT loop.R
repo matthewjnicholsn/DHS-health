7de1a3978f27fd7ca5d1dc34ee4d271dae4a5ca3
matthewjnicholsn <nichom18@mcmaster.ca>
2025-06-02 10:14:22 -0400

    Merge issue resolved. Can pass script into the CMORT loop now

---
+++ debugging CMORT loop.R
@@ -0,0 +1,34 @@
+rm(list = ls(all = TRUE))
+library(haven)
+library(dplyr)
+library(openxlsx)
+library(naniar)#for replace_with_na fx
+library(sjlabelled) #for fx set_label
+library(tidyr)
+
+#laod WI data for 1990 DHS
+WI <- read_dta("/Users/matthewnicholson/DHS/Nigeria\ DHS\'s/NG_1990_DHS_04072025_2113_219655/NGWI21DT/NGWI21FL.DTA")
+WI <- WI %>% 
+  select(-wlthindf) %>% 
+  rename(whhid, hhid = whhid) %>% 
+  mutate(hhid = as.numeric(gsub(" ", "", hhid)))
+
+BR <- read_dta("/Users/matthewnicholson/DHS/Nigeria\ DHS\'s/NG_1990_DHS_04072025_2113_219655/NGBR21DT/NGBR21FL.dta")
+BR <- BR %>% 
+  mutate(gsub('.{2}$', '', BR$caseid)) %>% 
+  rename(`gsub(".{2}$", "", BR$caseid)`, hhid = `gsub(".{2}$", "", BR$caseid)`) %>% 
+  mutate(hhid = as.numeric(gsub(" ", "", hhid))) %>% 
+  left_join(WI, by = "hhid") %>% 
+  rename(v190 = wlthind5) %>% 
+  left_join(WI %>% distinct(hhid, wlthind5), join_by(hhid), 
+            relationship = "many-to-many")
+#fixed the issue with merging WI to BR, will pass it to the CMORT loop file
+
+print(BR$v190)
+
+
+ mean(BR$v190
+      )
+mean(WI$v190) 
+mean(WI$wlthind5)
+weighted.mean(BR$v190, BR$v005, na.rm=T)
