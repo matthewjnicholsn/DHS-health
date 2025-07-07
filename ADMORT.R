af49fbe0e0e785a1745fc0c00b0ceda21aca34da
matthewjnicholsn <nichom18@mcmaster.ca>
2025-07-07 10:15:16 -0400

    attempting to debug the broken admort func and asmr helper func

--- ADMORT.R
+++ ADMORT.R
@@ -32,6 +32,7 @@ rm(list = ls())
 library(haven)
 library(openxlsx)
 library(DHS.rates)
+library(survey)
 
 
 IRdata <- read_dta("/Users/matthewnicholson/DHS/Nigeria\ DHS\'s/NG_2018_DHS_04072025_2116_219655/NGIR7BDT/NGIR7BFL.DTA")
@@ -56,128 +57,128 @@ varlist <- c("asmr")
 
 # name our own admort function
 
-admort <- function(Data.Name, Indicator, JK = NULL, CL = NULL, Strata = NULL, 
+admort <- function(IRdata_AMORT, Indicator, JK = NULL, CL = NULL, Strata = NULL, 
                     Cluster = NULL, Weight = NULL, Date_of_interview = NULL, 
                     PeriodEnd = NULL, Period = NULL) 
 {
   if (!Indicator %in% c("asmr")) 
     stop("Please specify a valid adult mortality indicator, such as asmr, aamr, asmmr, aammr, asprmr, mmr, prmr or aagfr")
   if (!is.null(Strata)) {
-    Data.Name$strata = Data.Name[[Strata]]
-    Data.Name$v022 = NULL
-    names(Data.Name)[names(Data.Name) == c("strata")] <- c("v022")
+    IRdata_AMORT$strata = IRdata_AMORT[[Strata]]
+    IRdata_AMORT$v022 = NULL
+    names(IRdata_AMORT)[names(IRdata_AMORT) == c("strata")] <- c("v022")
   }
   if (!is.null(Cluster)) {
-    Data.Name$cluster = Data.Name[[Cluster]]
-    Data.Name$v021 = NULL
-    names(Data.Name)[names(Data.Name) == c("cluster")] <- c("v021")
+    IRdata_AMORT$cluster = IRdata_AMORT[[Cluster]]
+    IRdata_AMORT$v021 = NULL
+    names(IRdata_AMORT)[names(IRdata_AMORT) == c("cluster")] <- c("v021")
   }
   if (!is.null(Weight)) {
-    Data.Name$weight = Data.Name[[Weight]]
-    Data.Name$v005 = NULL
-    names(Data.Name)[names(Data.Name) == c("weight")] <- c("v005")
+    IRdata_AMORT$weight = IRdata_AMORT[[Weight]]
+    IRdata_AMORT$v005 = NULL
+    names(IRdata_AMORT)[names(IRdata_AMORT) == c("weight")] <- c("v005")
   }
   if (!is.null(Date_of_interview)) {
-    Data.Name$DOI = Data.Name[[Date_of_interview]]
-    Data.Name$v008 = NULL
-    names(Data.Name)[names(Data.Name) == c("DOI")] <- c("v008")
+    IRdata_AMORT$DOI = IRdata_AMORT[[Date_of_interview]]
+    IRdata_AMORT$v008 = NULL
+    names(IRdata_AMORT)[names(IRdata_AMORT) == c("DOI")] <- c("v008")
   }
-  if (!("v021" %in% names(Data.Name))) 
+  if (!("v021" %in% names(IRdata_AMORT))) 
     stop({
       message("Error: v021/Primary-sampling-unit is missing")
     })
-  if (!("v005" %in% names(Data.Name))) 
+  if (!("v005" %in% names(IRdata_AMORT))) 
     stop({
       message("Error: v005/Sample-weight is missing")
     })
-  if (!("v008" %in% names(Data.Name))) 
+  if (!("v008" %in% names(IRdata_AMORT))) 
     stop({
       message("Error: v008/Date-of-Interview is missing")
     })
-  if (!("v022" %in% names(Data.Name))) 
+  if (!("v022" %in% names(IRdata_AMORT))) 
     stop({
       message("Error: v022/Sample-strata is missing")
     })
   else {
     if (("TRUE" %in% (!(paste("mm1_0", 1:9, sep = "") %in% 
-                        names(Data.Name)))) | ("TRUE" %in% (!(paste("mm1_", 
-                                                                    10:20, sep = "") %in% names(Data.Name))))) {
+                        names(IRdata_AMORT)))) | ("TRUE" %in% (!(paste("mm1_", 
+                                                                    10:20, sep = "") %in% names(IRdata_AMORT))))) {
       warning("Siblings variables mm1_01:mm1_20 are not complete; the missing variables were created")
     }
     if (("TRUE" %in% (!(paste("mm2_0", 1:9, sep = "") %in% 
-                        names(Data.Name)))) | ("TRUE" %in% (!(paste("mm2_", 
-                                                                    10:20, sep = "") %in% names(Data.Name))))) {
+                        names(IRdata_AMORT)))) | ("TRUE" %in% (!(paste("mm2_", 
+                                                                    10:20, sep = "") %in% names(IRdata_AMORT))))) {
       warning("Siblings variables mm2_01:mm2_20 are not complete; the missing variables were created")
     }
     if (("TRUE" %in% (!(paste("mm4_0", 1:9, sep = "") %in% 
-                        names(Data.Name)))) | ("TRUE" %in% (!(paste("mm4_", 
-                                                                    10:20, sep = "") %in% names(Data.Name))))) {
+                        names(IRdata_AMORT)))) | ("TRUE" %in% (!(paste("mm4_", 
+                                                                    10:20, sep = "") %in% names(IRdata_AMORT))))) {
       warning("Siblings variables mm4_01:mm4_20 are not complete; the missing variables were created")
     }
     if (("TRUE" %in% (!(paste("mm8_0", 1:9, sep = "") %in% 
-                        names(Data.Name)))) | ("TRUE" %in% (!(paste("mm8_", 
-                                                                    10:20, sep = "") %in% names(Data.Name))))) {
+                        names(IRdata_AMORT)))) | ("TRUE" %in% (!(paste("mm8_", 
+                                                                    10:20, sep = "") %in% names(IRdata_AMORT))))) {
       warning("Siblings variables mm8_01:mm8_20 are not complete; the missing variables were created")
     }
     if (("TRUE" %in% (!(paste("mm9_0", 1:9, sep = "") %in% 
-                        names(Data.Name)))) | ("TRUE" %in% (!(paste("mm9_", 
-                                                                    10:20, sep = "") %in% names(Data.Name))))) {
+                        names(IRdata_AMORT)))) | ("TRUE" %in% (!(paste("mm9_", 
+                                                                    10:20, sep = "") %in% names(IRdata_AMORT))))) {
       warning("Siblings variables mm9_01:mm9_20 are not complete; the missing variables were created")
     }
     if (("TRUE" %in% (!(paste("mm12_0", 1:9, sep = "") %in% 
-                        names(Data.Name)))) | ("TRUE" %in% (!(paste("mm12_", 
-                                                                    10:20, sep = "") %in% names(Data.Name))))) {
+                        names(IRdata_AMORT)))) | ("TRUE" %in% (!(paste("mm12_", 
+                                                                    10:20, sep = "") %in% names(IRdata_AMORT))))) {
       warning("Siblings variables mm12_01:mm12_20 are not complete; the missing variables were created")
     }
   }
   for (i in 1:9) {
-    if ("TRUE" %in% (!(paste("mm1_0", i, sep = "") %in% names(Data.Name)))) 
-      Data.Name[[paste("mm1_0", i, sep = "")]] <- NA
-    if ("TRUE" %in% (!(paste("mm2_0", i, sep = "") %in% names(Data.Name)))) 
-      Data.Name[[paste("mm2_0", i, sep = "")]] <- NA
-    if ("TRUE" %in% (!(paste("mm4_0", i, sep = "") %in% names(Data.Name)))) 
-      Data.Name[[paste("mm4_0", i, sep = "")]] <- NA
-    if ("TRUE" %in% (!(paste("mm8_0", i, sep = "") %in% names(Data.Name)))) 
-      Data.Name[[paste("mm8_0", i, sep = "")]] <- NA
-    if ("TRUE" %in% (!(paste("mm9_0", i, sep = "") %in% names(Data.Name)))) 
-      Data.Name[[paste("mm9_0", i, sep = "")]] <- NA
+    if ("TRUE" %in% (!(paste("mm1_0", i, sep = "") %in% names(IRdata_AMORT)))) 
+      IRdata_AMORT[[paste("mm1_0", i, sep = "")]] <- NA
+    if ("TRUE" %in% (!(paste("mm2_0", i, sep = "") %in% names(IRdata_AMORT)))) 
+      IRdata_AMORT[[paste("mm2_0", i, sep = "")]] <- NA
+    if ("TRUE" %in% (!(paste("mm4_0", i, sep = "") %in% names(IRdata_AMORT)))) 
+      IRdata_AMORT[[paste("mm4_0", i, sep = "")]] <- NA
+    if ("TRUE" %in% (!(paste("mm8_0", i, sep = "") %in% names(IRdata_AMORT)))) 
+      IRdata_AMORT[[paste("mm8_0", i, sep = "")]] <- NA
+    if ("TRUE" %in% (!(paste("mm9_0", i, sep = "") %in% names(IRdata_AMORT)))) 
+      IRdata_AMORT[[paste("mm9_0", i, sep = "")]] <- NA
     if ("TRUE" %in% (!(paste("mm12_0", i, sep = "") %in% 
-                       names(Data.Name)))) 
-      Data.Name[[paste("mm12_0", i, sep = "")]] <- NA
+                       names(IRdata_AMORT)))) 
+      IRdata_AMORT[[paste("mm12_0", i, sep = "")]] <- NA
     if ("TRUE" %in% (!(paste("mm16_0", i, sep = "") %in% 
-                       names(Data.Name)))) 
-      Data.Name$PRMMRT <- NA
+                       names(IRdata_AMORT)))) 
+      IRdata_AMORT$PRMMRT <- NA
     if ("TRUE" %in% (!(paste("mm16_0", i, sep = "") %in% 
-                       names(Data.Name)))) 
-      Data.Name[[paste("mm16_0", i, sep = "")]] <- NA
+                       names(IRdata_AMORT)))) 
+      IRdata_AMORT[[paste("mm16_0", i, sep = "")]] <- NA
   }
   for (i in 10:20) {
-    if ("TRUE" %in% (!(paste("mm1_", i, sep = "") %in% names(Data.Name)))) 
-      Data.Name[[paste("mm1_", i, sep = "")]] <- NA
-    if ("TRUE" %in% (!(paste("mm2_", i, sep = "") %in% names(Data.Name)))) 
-      Data.Name[[paste("mm2_", i, sep = "")]] <- NA
-    if ("TRUE" %in% (!(paste("mm4_", i, sep = "") %in% names(Data.Name)))) 
-      Data.Name[[paste("mm4_", i, sep = "")]] <- NA
-    if ("TRUE" %in% (!(paste("mm8_", i, sep = "") %in% names(Data.Name)))) 
-      Data.Name[[paste("mm8_", i, sep = "")]] <- NA
-    if ("TRUE" %in% (!(paste("mm9_", i, sep = "") %in% names(Data.Name)))) 
-      Data.Name[[paste("mm9_", i, sep = "")]] <- NA
-    if ("TRUE" %in% (!(paste("mm12_", i, sep = "") %in% names(Data.Name)))) 
-      Data.Name[[paste("mm12_", i, sep = "")]] <- NA
-    if ("TRUE" %in% (!(paste("mm16_", i, sep = "") %in% names(Data.Name)))) 
-      Data.Name[[paste("mm16_", i, sep = "")]] <- NA
+    if ("TRUE" %in% (!(paste("mm1_", i, sep = "") %in% names(IRdata_AMORT)))) 
+      IRdata_AMORT[[paste("mm1_", i, sep = "")]] <- NA
+    if ("TRUE" %in% (!(paste("mm2_", i, sep = "") %in% names(IRdata_AMORT)))) 
+      IRdata_AMORT[[paste("mm2_", i, sep = "")]] <- NA
+    if ("TRUE" %in% (!(paste("mm4_", i, sep = "") %in% names(IRdata_AMORT)))) 
+      IRdata_AMORT[[paste("mm4_", i, sep = "")]] <- NA
+    if ("TRUE" %in% (!(paste("mm8_", i, sep = "") %in% names(IRdata_AMORT)))) 
+      IRdata_AMORT[[paste("mm8_", i, sep = "")]] <- NA
+    if ("TRUE" %in% (!(paste("mm9_", i, sep = "") %in% names(IRdata_AMORT)))) 
+      IRdata_AMORT[[paste("mm9_", i, sep = "")]] <- NA
+    if ("TRUE" %in% (!(paste("mm12_", i, sep = "") %in% names(IRdata_AMORT)))) 
+      IRdata_AMORT[[paste("mm12_", i, sep = "")]] <- NA
+    if ("TRUE" %in% (!(paste("mm16_", i, sep = "") %in% names(IRdata_AMORT)))) 
+      IRdata_AMORT[[paste("mm16_", i, sep = "")]] <- NA
   }
-  Data.Name <- as.data.frame(Data.Name)
+  IRdata_AMORT <- as.data.frame(IRdata_AMORT)
   if (Indicator == "asmr") {
-    getFromNamespace("ASMR", "DHS.rates")(Data.Name, CL, PeriodEnd, Period)[[1]]
+    getFromNamespace("ASMR", "DHS.rates")(IRdata_AMORT, CL, PeriodEnd, Period)[[1]]
   }
 }
 
 #define ASMR function to bypass the DHSrates function
-ASMR <- function (Data.Name, CL = NULL, PeriodEnd = NULL, Period = NULL) 
+ASMR <- function (IRdata_AMORT, CL = NULL, PeriodEnd = NULL, Period = NULL) 
 {
-  Data.Name <- Data.Name[!Data.Name$v005 == 0, ]
-  Data.Name$ID <- seq.int(nrow(Data.Name))
+  IRdata_AMORT <- IRdata_AMORT[!IRdata_AMORT$v005 == 0, ]
+  IRdata_AMORT$ID <- seq.int(nrow(IRdata_AMORT))
   if (is.null(CL)) {
     Z <- stats::qnorm(0.025, lower.tail = FALSE)
   }
@@ -191,22 +192,22 @@ ASMR <- function (Data.Name, CL = NULL, PeriodEnd = NULL, Period = NULL)
     Periodmsg = Period
   }
   if (is.null(PeriodEnd)) {
-    PeriodEndy_ <- as.integer((mean(Data.Name$v008) - 1)/12) + 
+    PeriodEndy_ <- as.integer((mean(IRdata_AMORT$v008) - 1)/12) + 
       1900
-    PeriodEndm_ <- round(mean(Data.Name$v008) - ((PeriodEndy_ - 
+    PeriodEndm_ <- round(mean(IRdata_AMORT$v008) - ((PeriodEndy_ - 
                                                     1900) * 12), 0)
-    PeriodEndm_m <- round(min(Data.Name$v008) - ((PeriodEndy_ - 
+    PeriodEndm_m <- round(min(IRdata_AMORT$v008) - ((PeriodEndy_ - 
                                                     1900) * 12), 0)
-    PeriodEndm_x <- round(max(Data.Name$v008) - ((PeriodEndy_ - 
+    PeriodEndm_x <- round(max(IRdata_AMORT$v008) - ((PeriodEndy_ - 
                                                     1900) * 12), 0)
   }
   else {
     dates <- paste(PeriodEnd, "01", sep = "-")
     PeriodEndm_ <- as.numeric(format(as.Date(dates), "%m"))
     PeriodEndy_ <- as.numeric(format(as.Date(dates), "%Y"))
-    if (PeriodEndm_ >= round(mean(Data.Name$v008) - (((as.integer((mean(Data.Name$v008) - 
+    if (PeriodEndm_ >= round(mean(IRdata_AMORT$v008) - (((as.integer((mean(IRdata_AMORT$v008) - 
                                                                    1)/12) + 1900) - 1900) * 12), 0) & PeriodEndy_ >= 
-        as.integer((mean(Data.Name$v008) - 1)/12) + 1900) 
+        as.integer((mean(IRdata_AMORT$v008) - 1)/12) + 1900) 
       message(crayon::bold("Note:", "\n", "You specified a reference period that ends after the survey fieldwork dates....."), 
               "\n", "1. Make sure the dates in the survey are coded according to the Gregorian calendar.", 
               "\n", "2. If the dates are coded according to the Gregorian calendar, use a proper PeriodEnd that came before the time of the survey.", 
@@ -233,8 +234,8 @@ ASMR <- function (Data.Name, CL = NULL, PeriodEnd = NULL, Period = NULL)
         crayon::red$bold$underline(round((PeriodEndy_ + PeriodEndm_/12) - 
                                            (Periodmsg/24), digits = 2)), "\n")
   }
-  Data.Name$allwoment = 1
-  DeathEx <- DataPrepareM(Data.Name, PeriodEnd, Period)
+  IRdata_AMORT$allwoment = 1
+  DeathEx <- DataPrepareM(IRdata_AMORT, PeriodEnd, Period)
   options(survey.lonely.psu = "adjust")
   vars_needed <- c("v021", "v022", "rweight")
   for (v in vars_needed) {
