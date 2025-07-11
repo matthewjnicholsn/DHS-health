f0a5437e55e8e4efb5d42f46ce4041e7460cf36c
matthewjnicholsn <nichom18@mcmaster.ca>
2025-07-11 13:07:04 -0400

    attempt at writing a working admort script that loops through several DHS cycles. Still work to be done in making the functions compatible across cycles.

---
+++ attempted_admort_script.R
@@ -0,0 +1,41 @@
+## This a-script is a gonna be one a spicy meatball! (it will be a loop that runs mort calcs on every cycle of Ng DHS)
+## load libraries
+rm(list = ls())
+package_list <- c("haven", "dplyr", "tidyr", "stringr", "survey", "openxlsx")
+lapply(package_list, require, character.only = T)
+
+#load in the function scripts
+source("/Users/matthewnicholson/DHS/admort_scripts/simplified_admort_func.R")
+source("/Users/matthewnicholson/DHS/admort_scripts/debug_helper_funs_loop.R")
+source("/Users/matthewnicholson/DHS/admort_scripts/DataPrepareGrpM_func.R")
+#blank list for results
+admort_results <- list()
+#file list
+file_list <- c("/Users/matthewnicholson/DHS/Nigeria\ DHS\'s/NG_1990_DHS_04072025_2113_219655/NGIR21DT/NGIR21FL.DTA", 
+               "/Users/matthewnicholson/DHS/Nigeria\ DHS\'s/NG_2003_DHS_04072025_2113_219655/NGIR4BDT/NGIR4BFL.DTA", 
+               "/Users/matthewnicholson/DHS/Nigeria\ DHS\'s/NG_2008_DHS_04072025_2113_219655/NGBR53DT/NGBR53FL.DTA", 
+               "/Users/matthewnicholson/DHS/Nigeria\ DHS\'s/NG_2013_DHS_04072025_2114_219655/NGIR6ADT/NGIR6AFL.DTA", 
+               "/Users/matthewnicholson/DHS/Nigeria\ DHS\'s/NG_2018_DHS_04072025_2116_219655/NGIR7BDT/NGIR7BFL.DTA")
+#create the results list
+admort_results <- vector("list", 5)
+#list indicators
+varlist <- c("asmr", "aamr", "asprmr", "aaprmr", "prmr")
+
+
+
+for(i in seq_along(file_list)){
+  ir_data_file <- file_list[i]
+  ir_data <- read_dta(ir_data_file) 
+  
+  #runs admort
+  for (i in seq_along(varlist)) {
+    
+    admort_results[[i]]  <- as.data.frame(simplified_admort_func(ir_data,Indicator=varlist[i]))
+    
+  }
+  
+  admort_results[[basename(ir_data_file)]] <- admort_results 
+  admort_results_final <- do.call(rbind, admort_results)
+  
+  }
+
