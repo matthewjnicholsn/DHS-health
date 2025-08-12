3c1d718a42778f0a57e4e326effd0443895638df
matthewjnicholsn <nichom18@mcmaster.ca>
2025-08-11 20:41:38 -0400

    simplified the script by making te dhs function debug script a function

--- admort_script_loop.R
+++ admort_script_loop.R
@@ -23,44 +23,21 @@ source("/Users/matthewnicholson/DHS/admort_scripts/simplified_admort_func.R")
 indicator_list <- c("asmr", "aamr", "asmmr", "aammr", "asprmr", 
                     "aaprmr", "prmr", "mmr" 
                     # ,"aagfr"
-) %>% str_to_upper()
+)
 
-for (i in seq_along(indicator_list)) {
-  
-  
-  indicator <- indicator_list[[i]]
-  # get the function for each indicator
-  fun <- get(indicator, envir = asNamespace("DHS.rates"))
-  
-  # Convert function body to text
-  fun_lines <- deparse(body(fun))
-  
-  # Find the line starting with 'dstrat'
-  dstrat_line <- grep("^\\s*dstrat", fun_lines)[1]
-  
-  # The line to insert
-  insert_line <- "DeathEx <- subset(DeathEx, !is.na(v021) & !is.na(v022) & !is.na(rweight))"
-  
-  # Insert your line before the dstrat line
-  if (!is.na(dstrat_line)) {
-    fun_lines <- append(fun_lines, insert_line, after = dstrat_line - 1)
-  } else {
-    warning(paste("No dstrat line found in", indicator))
-  }
-  # reconstruct the function with the same arguments as the original
-  # get the arguments from the original function
-  args <- paste(names(formals(fun)), collapse = ", ")
-  fun_def <- sprintf("function(%s) {\n%s\n}", args, paste(fun_lines, collapse = "\n"))
-  new_fun <- eval(parse(text = fun_def))
-  
-  # assign the new function to the environment with the same name as the indicator 
-  assign(indicator, new_fun, envir = .GlobalEnv)
-}
+source("patch_dhsrate_functions.R")
+patch_dhsrate_functions(str_to_upper(indicator_list))
+
+
 #source a few more indicators (ones not needing debug)
-AAGFR <- DHS.rates:::AAGFR
-DataPrepareM_GFR <- DHS.rates:::DataPrepareM_GFR
-fert <- DHS.rates::fert
+add_indicators <- c("AAGFR", "DataPrepareM_GFR", "fert")
+for(i in add_indicators){
+  assign(i, get(i, envir = asNamespace("DHS.rates")), envir = .GlobalEnv)
+}
+
+#emty list for results
 all_results <- list()
+#intiate loop
 for(i in seq_along(file_list)){
   ir_data <- read_dta(file_list[i])
   # TFR7 <- as.data.frame(fert(ir_data, Indicator = "tfr", Period = 84, EverMW = "Yes",
