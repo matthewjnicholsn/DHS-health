cc92cddc1b7c3cab999b680630be2086de788e93
matthewjnicholsn <nichom18@mcmaster.ca>
2025-09-27 14:39:35 -0400

    trying to make kriging work within national boundaries, this code causes the covariance matrices to all collapse to 0, so inversion yields NA values

--- chmort_cluster_kriging.R
+++ chmort_cluster_kriging.R
@@ -1,4 +1,5 @@
-lib_list <- c("sp","sf", "gstat", "ggplot2", "viridis", "haven", "dplyr", "stringr", "purrr", "DHS.rates", "sjlabelled")
+rm(list = ls())
+lib_list <- c("sp","sf", "beepr", "gstat", "ggplot2", "viridis", "haven", "dplyr", "stringr", "purrr", "DHS.rates", "sjlabelled")
 lapply(lib_list, require, character.only = T)
 #get file function
 source("/Users/matthewnicholson/DHS/get_file_function.R")
@@ -97,15 +98,27 @@ BRdata_CMORT <- (BRdata[, c("v001", "v021", "v022","v024", "v025", "v005", "v008
     rename(cluster = Class)
 
 
-write.csv(CHMORT, file = "Tables_child_mort_Ng1990_cluster.csv", append=TRUE)
+write.csv(res_clust, file = "Tables_child_mort_Ng1990_cluster.csv")
 
 ########################################################################################
-
+#test region 1 first
 #now to prepare for kriging
 #merge data with gps file to visualize
 res_clust <- res_clust |> 
   left_join(gps_data, by = "cluster") |> 
-  st_as_sf()
+  mutate(prob = as.numeric(as.character(prob)) * 1000) |> 
+  filter(region %in% c(1:4)) |> 
+  filter(cluster != 1402) |> 
+  st_as_sf() 
+#remove 0,0 coords
+#~~~~~~~~~~~#~~~~~~~~~~~#~~~~~~~~~~~#~~~~~~~~~~~#~~~~~~~~~~~#~~~~~~~~~~~#~~~~~~~~~~~#~~~~~~~~~~~#~~~~~~~~~~~
+
+#in this part I want to use this file: ng_1980_shp <- st_read("/Users/matthewnicholson/Downloads/Nigeria_states/1987-1991/1987-1991.shp"),
+# which has polygon geometry of nigeria to constrain the bounds of the kriging to the shape of nigeria. I will need to map only the outer lines as the file contains teh
+#geometries for each region. So the geoms will neeed to be aggregated and then the shape will be used as the bounds for the kriging and for the outline of the map (as a layer maybe)
+
+
+#~~~~~~~~~~~#~~~~~~~~~~~#~~~~~~~~~~~#~~~~~~~~~~~#~~~~~~~~~~~#~~~~~~~~~~~#~~~~~~~~~~~#~~~~~~~~~~~#~~~~~~~~~~~
 
 ggplot(data = res_clust) +
   geom_sf(aes(color = prob), size = 3) +
@@ -113,42 +126,66 @@ ggplot(data = res_clust) +
   theme_minimal() +
   labs(title = "Cluster-level child death probability (Nigeria 1990)")
 
-res_clust <- as(res_clust, "Spatial")
-res_clust <- res_clust[-zerodist(res_clust)[,1],]
-crs_clusters <- st_crs(res_clust)
+#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~#
+# CONSTRAINING KRIGING TO NIGERIA BOUNDARY
+#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~#
 
-# 1. Project your data first
-res_clust <- st_as_sf(res_clust)
-res_clust <- st_transform(res_clust, 32632) # UTM projection
+# dissolve shapefile to single Nigeria outline
+nigeria_outline <- shp_file |> st_union() |> st_make_valid()
 
-# 2. Create grid AFTER projection, with fixed resolution
-bbox <- st_bbox(res_clust)
-res <- 1000   # cell size in meters (10 km, adjust as needed)
+# project to same CRS as cluster data
+res_clust <- st_transform(res_clust, 32632) # UTM
+nigeria_outline <- st_transform(nigeria_outline, 32632)
+# plot(nigeria_outline) to check geometry union worked. Will take a long time as it's a large shapefile
+
+# create grid inside bounding box
+bbox <- st_bbox(nigeria_outline)
+res <- 1000   # 10 km grid resolution (adjust as needed)
 
 x.range <- seq(bbox$xmin, bbox$xmax, by = res)
 y.range <- seq(bbox$ymin, bbox$ymax, by = res)
 grid <- expand.grid(x = x.range, y = y.range)
 
 grid_sf <- st_as_sf(grid, coords = c("x", "y"), crs = 32632)
+
+# keep only grid points inside Nigeria
+grid_sf <- grid_sf[nigeria_outline, ]
+
+# convert to Spatial for gstat
+res_clust_sp <- as(res_clust, "Spatial")
 grid_sp <- as(grid_sf, "Spatial")
 
-# 3. Convert cluster data back to Spatial
-res_clust_sp <- as(res_clust, "Spatial")
-
-# 4. Add coords for variogram/kriging
+# add coords
 res_clust_sp$X <- coordinates(res_clust_sp)[,1]
 res_clust_sp$Y <- coordinates(res_clust_sp)[,2]
 grid_sp$X <- coordinates(grid_sp)[,1]
 grid_sp$Y <- coordinates(grid_sp)[,2]
 
-# 5. Variogram + kriging
+# variogram + kriging
 vgm_emp <- variogram(prob ~ 1, res_clust_sp)
 vgm_fit <- fit.variogram(vgm_emp, model = vgm(c("Exp","Mat","Gau","Sph")))
+plot(vgm_emp, vgm_fit)
 
 k_model <- gstat(formula = prob ~ 1,
                  data = res_clust_sp,
                  model = vgm_fit)
 
 k_pred <- predict(k_model, grid_sp)
-## need to try to scale the chmort to deaths per 1000 ie just multiply prob by 1000. Then could scale to 0 mean... 
-# as of right now semivariance is far far too small for the size of distance we are working with
+beep()
+
+# convert predictions for ggplot
+k_pred_df <- as.data.frame(k_pred)
+coords <- coordinates(k_pred)
+k_pred_df$x <- coords[,1]
+k_pred_df$y <- coords[,2]
+
+dim(k_pred_df)
+# plot with Nigeria boundary
+ggplot() +
+  geom_tile(data = k_pred_df, aes(x = x, y = y, fill = var1.pred)) +
+  # geom_sf(data = nigeria_outline, fill = NA, color = "black", linewidth = 0.6) +
+  scale_fill_viridis_c(name = "Predicted child mortality", option = "F") +
+  coord_sf() +
+  theme_minimal() +
+  labs(title = "Ordinary kriging: child mortality (1990), constrained to Nigeria")
+beep()
